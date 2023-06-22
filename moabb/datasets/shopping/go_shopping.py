7bfe18caeb5af6a915251db94712ff3b4b9b7131
Gregoire Cattan <gregoire.cattan@ibm.com>
2023-06-22 22:03:31 +0200

    add go_shopping dataset

---
+++ moabb/datasets/shopping/go_shopping.py
@@ -0,0 +1,68 @@
+"""
+Alex Motor imagery dataset.
+"""
+
+from os import sched_rr_get_interval
+from mne.io import Raw
+from moabb.datasets.braininvaders import VirtualReality
+
+from .. import download as dl
+from ..base import BaseDataset
+
+
+ALEX_URL = "https://zenodo.org/record/806023/files/"
+
+
+class GoShoppingDataset(BaseDataset):
+    """TODO
+    selection:
+        {
+            subject1: (dataset1, subject, session, run)
+            subject2:
+        }
+    """
+
+    @property
+    def count(self):
+        return len(self.selection.keys())
+
+    def __init__(self, selection: dict, events: dict, code: str, interval: list, paradigm: str):
+        self.selection = selection
+        super().__init__(
+            subjects=list(range(1, self.count + 1)),
+            sessions_per_subject=1,
+            events=events,
+            code=code,
+            interval=interval,
+            paradigm=paradigm,
+        )
+
+    def _get_single_subject_data(self, shopped_subject):
+        """return data for a single subject"""
+        dataset, subject, session, run = self.selection[shopped_subject]
+        raw = dataset._get_single_subject_data(subject)[session][run]
+
+        return {"session_0": {"run_0": raw}}
+
+    def data_path(
+        self, shopped_subject, path=None, force_update=False, update_path=None, verbose=None
+    ):
+        dataset, subject, _, _ = self.selection[shopped_subject]
+        return dataset.data_path(subject)
+
+class BiWithIlliteracy(GoShoppingDataset):
+    def __init__(self):
+        biVR = VirtualReality(virtual_reality=True, screen_display=True)
+        selection = {
+            0: (biVR, 1, 'VR', 'block_1-repetition_1'),
+            1: (biVR, 1, 'VR', 'block_1-repetition_2')
+        }
+        GoShoppingDataset.__init__(
+            self,
+            selection=selection,
+            events=dict(Target=2, NonTarget=1),
+            code="BI-ILL",
+            interval=[0, 1.0],
+            paradigm="p300"
+        )
+ 
