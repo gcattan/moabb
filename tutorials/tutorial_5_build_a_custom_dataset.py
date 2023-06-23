045d210faa7a0eb80f421a6ca16670dd0115d3ac
Gregoire Cattan <gregoire.cattan@ibm.com>
2023-06-23 10:41:01 +0200

    add supports for run indices

--- tutorials/tutorial_5_build_a_custom_dataset.py
+++ tutorials/tutorial_5_build_a_custom_dataset.py
@@ -11,7 +11,7 @@ from moabb.datasets import VirtualReality
 from moabb.datasets.shopping import GoShoppingDataset
 from moabb.paradigms.p300 import P300
 from pyriemann.classification import MDM
-from pyriemann.estimation import Covariances
+from pyriemann.estimation import Covariances, ERPCovariances, XdawnCovariances
 from sklearn.pipeline import make_pipeline
 
 from moabb.datasets import download as dl
@@ -31,13 +31,15 @@ from moabb.paradigms import LeftRightImagery
 # The fake dataset is available on the
 # `Zenodo website <https://sandbox.zenodo.org/record/369543>`_
 
+def block_rep(blocks: list, reps: list):
+    return [f'block_{b}-repetition_{r}' for b in blocks for r in reps]
 
 class BiWithIlliteracy(GoShoppingDataset):
     def __init__(self):
         biVR = VirtualReality(virtual_reality=True, screen_display=True)
         selection = {
-            1: (biVR, 1, 'VR', None),
-            2: (biVR, 2, 'VR', None)
+            1: (biVR, 1, 'VR', block_rep([1,2], [1,2,3,4,5])),
+            2: (biVR, 2, 'VR', 'block_1-repetition_1'),
         }
         GoShoppingDataset.__init__(
             self,
@@ -55,7 +57,7 @@ evaluation = WithinSessionEvaluation(
     paradigm=paradigm, datasets=datasets, overwrite=False, suffix="newdataset"
 )
 pipelines = {}
-pipelines["MDM"] = make_pipeline(Covariances("oas"), MDM(metric="riemann"))
+pipelines["MDM"] = make_pipeline(ERPCovariances(), MDM(metric="riemann"))
 scores = evaluation.process(pipelines)
 
 print(scores)
